8fa786104fe8afcc3ef1103d8299f70e7c7289bf
gszxz <guisouza0676@gmail.com>
2026-04-08 10:37:47 -0300

    Update api.js

--- frontend/meu-projeto/src/services/api.js
+++ frontend/meu-projeto/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: 'https://projectapp1-production.up.railway.app'
+  baseURL: 'https://projectapp1-production.up.railway.app:3000'
 })
 export default api
